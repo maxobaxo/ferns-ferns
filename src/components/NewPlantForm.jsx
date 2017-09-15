ac112f24972a6b59a329454ada15c6b2f21e1dc8
Max Scher <maxobaxo@gmail.com>
2017-09-15 09:10:12 -0700

    add local function addNewPlant() to NewPlantForm

--- src/components/NewPlantForm.jsx
+++ src/components/NewPlantForm.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
+import Plant from '../models/plant';
 
-function NewPlantForm(props) {
+class NewPlantForm extends React.Component {
 
-  return(
-    <div>
-      <form>
-        <input placeholder="Name"/>
-        <input placeholder="Description"/>
-        <input placeholder="Category"/>
-        <select>
-          <option value="1">Every Day</option>
-          <option value="2">Every Other Day</option>
-          <option value="7">Once a Week</option>
-        </select>
-        <button type="submit" class="btn">Add Plant</button>
-      </form>
-    </div>
-  )
+  constructor(props) {
+    super(props);
+    this.addNewPlant = this.addNewPlant.bind(this);
+  }
+
+  addNewPlant(event) {
+    event.preventDefault();
+    const { _name, _description, _category, _whenToWater } = this.refs;
+    var newPlant = new Plant(_name.value, _description.value, _category.value, _whenToWater.value);
+  }
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.addNewPlant}>
+          <input id="name" ref="_name" placeholder="Name"/>
+          <input id="description" ref="_description" placeholder="Description"/>
+          <input id="category" ref="_category" placeholder="Category"/>
+          <select id="whenToWater" ref="_whenToWater">
+            <option value="1">Every Day</option>
+            <option value="2">Every Other Day</option>
+            <option value="7">Once a Week</option>
+          </select>
+          <button type="submit" class="btn">Add Plant</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewPlantForm;
