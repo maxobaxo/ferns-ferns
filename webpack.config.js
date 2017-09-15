b95998b467b325d7f52a284f157b18c57d581232
Max Scher <maxobaxo@gmail.com>
2017-09-15 08:29:04 -0700

    add title to webpack config module

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'template.ejs',
       appMountId: 'react-app-root',
-      title: '',
+      title: 'Ferns',
       filename: resolve(__dirname, 'build', 'index.html'),
     }),
   ]
