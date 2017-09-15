66fc9ebaa24fbc87f18033e4a9a186427c033e00
Max Scher <maxobaxo@gmail.com>
2017-09-15 08:12:54 -0700

    Add more dependencies
    Dependencies added: webpack-dev-server, html-webpack-plugin,
    react-hot-loader, prop-types.
    Modified template.ejs with proper syntax.
    Refactor index.jsx and webpack.config.js to accommodate new dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,33 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
   entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     resolve(__dirname, 'src') + '/index.jsx'
   ],
 
   output: {
     filename: 'app.bundle.js',
-    path: resolve(__dirname, 'build')
+    path: resolve(__dirname, 'build'),
+    publicPath: '/'
   },
 
   resolve: {
     extensions: ['.js', '.jsx']
   },
+
+  devtool: '#sourcemap',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/'
+  }
 
   module: {
     rules: [
@@ -24,11 +37,25 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: [
-            'es2015',
+            ['es2015', {'modules': false}],
             'react'
+          ],
+          plugins: [
+            'react-hot-loader/babel'
           ]
         }
       }
     ]
-  }
-}
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'template.ejs',
+      appMountId: 'react-app-root',
+      title: '',
+      filename: resolve(__dirname, 'build', 'index.html'),
+    }),
+  ]
+};
