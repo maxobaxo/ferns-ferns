3ca3b2b98a98570a5840829444377a5d10e4177f
Max Scher <maxobaxo@gmail.com>
2017-09-15 13:05:32 -0700

    incorporate the use of momentJS to calculate timeSinceWatered

--- src/models/plant.js
+++ src/models/plant.js
@@ -1,8 +1,16 @@
+import Moment from 'moment';
+
 class Plant {
   constructor(name, category, whenToWater) {
     this.name = name;
     this.category = category;
     this.whenToWater = whenToWater;
+    this.timeWatered = new Moment();
+    this.timeSinceWatered = "a few seconds";
+  }
+
+  setTimeSinceWatered() {
+    this.timeSinceWatered = this.timeWatered.fromNow(true);
   }
 }
 
